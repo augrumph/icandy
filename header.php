f5d77c52a71c4e4274ca56d50941b80d263d6269
Augusto <72392832+augrumph@users.noreply.github.com>
2021-05-16 20:54:38 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type="text/css" href="./css/header.css">
+    <script type="text/javascript" src="./js/index.js"></script>
+    <title>Document</title>
+</head>
+<body>
+     <div class="header">
+         
+        
+
+        <a><img src="./imgs/logo.jpeg" height="80px" width="220px"></a>
+        <div class="header-right">
+            <a class="menu" href="menuUsuario.php">HOME</a>
+            <a class="menu" href="produtos.php">PRODUTOS</a>
+            <a class="menu" href="carrinho.php">CARRINHO</a>
+            <a class="menu" href="logout.php">Sair</a>
+        </div>
+    </div>
+</body>
+</html>
