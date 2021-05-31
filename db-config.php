3537e0dafd6842d7d086d1455fa2f18dd674289d
carolalua <57256788+carolalua@users.noreply.github.com>
2021-05-31 20:28:08 -0300

    Delete db-config.php

--- db-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$databaseHost     = 'localhost';
-$databaseName     = 'icandy';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
